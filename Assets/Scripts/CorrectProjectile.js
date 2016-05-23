1ddc43ebde033c6d5438ffa6615ad7ade4cdc20e
PrimeDerektive <derek.s.traver@gmail.com>
2016-05-23 08:33:50 -0400

    added prelimnary rocket correction code

---
+++ Assets/Scripts/CorrectProjectile.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var correctTarget : Transform;
+var rb : Rigidbody;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+
+	Invoke("CorrectPosition", 1.0);
+}
+
+function FixedUpdate(){
+	if(correctTarget){
+		var dirToTarget = (correctTarget.position - transform.position).normalized;
+		transform.forward = Vector3.Lerp(transform.forward, dirToTarget, Time.deltaTime);
+		rb.velocity = transform.forward * 30.0;
+	}
+}
+
+function CorrectPosition(){
+	correctTarget = GameObject.Find("EnemyPrefab").transform;
+	//var correctPos = transform.position + transform.right * 5.0;
+	//GetComponent.<Rigidbody>().position = correctPos;
+}
--- Assets/Scripts/Weapons/Weapon_RocketLauncher.js
+++ Assets/Scripts/Weapons/Weapon_RocketLauncher.js
@@ -1,5 +1,11 @@
 ﻿#pragma strict
 
 class Weapon_RocketLauncher extends Weapon{
+
+	function EnableMuzzleFlash() : IEnumerator{
+
+		var newMuzzleFlash = GameObject.Instantiate(muzzleFlash, barrel.position, barrel.rotation);
+		newMuzzleFlash.transform.parent = barrel;
+	}
 	
 }
