b9cffcffe71fba3b185292d4e1bac8420befc712
PrimeDerektive <derek.s.traver@gmail.com>
2016-08-17 23:30:13 -0400

    added behaviourmachine

---
+++ Assets/Scripts/AI/States/Idle.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+import UnityEngine.Networking;
+
+public class Idle extends NetworkBehaviour{
+
+	//how often to try and find a target
+	var findTargetInterval : float = 0.5;
+
+	function OnEnable(){
+		InvokeRepeating("FindTarget", 0.0, findTargetInterval);
+	}
+
+	function FindTarget(){
+
+		//return if we're not the server
+		if(!isServer)
+			return;
+
+		//get the first object we find with the Player tag
+		var newTarget = GameObject.FindGameObjectWithTag("Player");
+
+		//if we find one
+		if(newTarget != null){
+			var newTargetNetId = newTarget.GetComponent.<NetworkIdentity>().netId;
+			Debug.Log("Found a target: " + newTargetNetId);
+		}
+
+	}
+
+	/*
+	@ClientRpc
+	function RpcSetTarget(){
+
+	}
+	*/
+
+}
